af183a458b07a54459f85d7c0721f6154335f015
explore <tfyt2009@sina.cn>
2020-10-30 18:44:58 +0800

    MybatisPlus CURD操作接口

--- mybatisplus/src/main/java/xin/altitude/mybatisplus/mybatisplus/entity/User.java
+++ mybatisplus/src/main/java/xin/altitude/mybatisplus/mybatisplus/entity/User.java
@@ -1,36 +1,60 @@
 package xin.altitude.mybatisplus.mybatisplus.entity;
 
-import java.io.Serializable;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.NoArgsConstructor;
 
-/**
- * <p>
- * 用户实体对应表 user
- * </p>
- *
- * @author hubin
- * @since 2018-08-11
- */
 @Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@TableName(value="sys_user")
 public class User {
-    /**
-     * 指定id生成算法
-     */
-    @TableId(type = IdType.ASSIGN_ID)
-    private Long uid;
-    private String name;
-    private Integer age;
-    private String email;
 
+	// 主键ID（全局唯一）
+	@ApiModelProperty(value = "主键ID（全局唯一）", position = 0)
+	@TableId
+	private Long id;
 
-    @TableField(select = false)
-    private Integer deleted;
-    @Version
-    private int version;
+	// 姓名
+	@ApiModelProperty(value = "姓名", position = 1)
+	private String name;
+
+	// 年龄
+	@ApiModelProperty(value = "年龄", position = 2)
+	private Integer age;
+
+	// 邮箱
+	@ApiModelProperty(value = "邮箱", position = 3)
+	private String email;
+
+	// 逻辑删除（0:未删除；1:已删除）
+	@ApiModelProperty(value = "逻辑删除（0:未删除；1:已删除）", position = 4)
+	@TableField(select = false)
+	private String deleted;
+
+	// 乐观锁
+	@ApiModelProperty(value = "乐观锁", position = 5)
+	@Version
+	private Integer version;
+
+	// 创建时间
+	@ApiModelProperty(value = "创建时间", position = 6)
+	private java.util.Date gmtCreate;
+
+	// 修改时间
+	@ApiModelProperty(value = "修改时间", position = 7)
+	private java.util.Date gmtModified;
+
+	public User(String name, Integer age, String email) {
+		this.name = name;
+		this.age = age;
+		this.email = email;
+	}
 }
