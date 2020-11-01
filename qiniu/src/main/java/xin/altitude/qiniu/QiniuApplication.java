e49e4d8330d57502a3095eee62989a26a41db6e7
explore <tfyt2009@sina.cn>
2020-11-01 15:15:19 +0800

    七牛云文件上传模块

---
+++ qiniu/src/main/java/xin/altitude/qiniu/QiniuApplication.java
@@ -0,0 +1,13 @@
+package xin.altitude.qiniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiniuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiniuApplication.class, args);
+	}
+
+}
---
+++ qiniu/src/test/java/xin/altitude/qiniu/QiniuApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.altitude.qiniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiniuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
