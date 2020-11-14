40dad3a7e80aa549c3126d76c8ad1e043fe0d794
explore <tfyt2009@sina.cn>
2020-11-14 17:03:25 +0800

    修改MP表转实体模版

--- mybatisplus/src/main/java/xin/altitude/mybatisplus/entity/Book.java
+++ mybatisplus/src/main/java/xin/altitude/mybatisplus/entity/Book.java
@@ -5,51 +5,33 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@TableName(value="")
+@TableName(value="tb_book")
+@JsonIgnoreProperties({"deleted", "gmtCreate", "gmtModified"})
 public class Book {
-
 	// 主键ID（全局唯一）
-	@ApiModelProperty(value = "主键ID（全局唯一）", position = 0)
 	@TableId
 	private Long id;
-
 	// 书名
-	@ApiModelProperty(value = "书名", position = 1)
 	private String name;
-
 	// 价格
-	@ApiModelProperty(value = "价格", position = 2)
 	private Integer price;
-
 	// 出版社
-	@ApiModelProperty(value = "出版社", position = 3)
 	private String press;
-
 	// 逻辑删除（0:未删除；1:已删除）
-	@ApiModelProperty(value = "逻辑删除（0:未删除；1:已删除）", position = 4)
-	@TableField(select = false)
-	private String deleted;
-
+	private Boolean deleted;
 	// 乐观锁
-	@ApiModelProperty(value = "乐观锁", position = 5)
 	@Version
 	private Integer version;
-
 	// 创建时间
-	@ApiModelProperty(value = "创建时间", position = 6)
 	private java.util.Date gmtCreate;
-
 	// 修改时间
-	@ApiModelProperty(value = "修改时间", position = 7)
 	private java.util.Date gmtModified;
-
-
 }
--- mybatisplus/src/main/java/xin/altitude/mybatisplus/service/BookService.java
+++ mybatisplus/src/main/java/xin/altitude/mybatisplus/service/BookService.java
@@ -1,6 +1,7 @@
 package xin.altitude.mybatisplus.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import xin.altitude.mybatisplus.entity.Book;
 
@@ -8,6 +9,5 @@ import xin.altitude.mybatisplus.entity.Book;
  * @author explore
  * @date 2020/10/30 16:37
  */
-@Service
 public interface BookService extends IService<Book> {
 }
