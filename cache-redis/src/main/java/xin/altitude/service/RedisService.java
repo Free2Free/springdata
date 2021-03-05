c874a2d78408fcdb030923a5ded9ae101b49f9fd
explore <982361244@qq.com>
2021-03-05 14:26:44 +0800

    修改redis缓存模块

--- cache-redis/src/main/java/xin/altitude/service/RedisService.java
+++ cache-redis/src/main/java/xin/altitude/service/RedisService.java
@@ -2,15 +2,16 @@ package xin.altitude.service;
 
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Condition;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 @Service
 @CacheConfig(cacheNames = "VIEW",keyGenerator = "customKeyGenerator")
 public class RedisService {
-    @Cacheable()
+    @Cacheable(unless = "#result==null")
     public String getData(Integer a, String b, Model model){
         System.out.println("第一次调用方法，第二次之后就直接从缓存取数据");
-        return "ABCD";
+        return null;
     }
 }
